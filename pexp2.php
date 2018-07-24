85b8b40f1e3123334b33a92e18e4c40b21fb2cc7
Nurilhijas <40590317+Nurilhijas@users.noreply.github.com>
2018-07-24 11:17:26 +0530

    Add files via upload

---
+++ pexp2.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $x = "Hello world!";
+$y = 'I am Nuril Hijas ';
+echo $x;
+echo "<br>"; 
+echo $y;echo "<br>"; 
+$x = 5985;
+var_dump($x);
+        ?>
+    </body>
+</html>
